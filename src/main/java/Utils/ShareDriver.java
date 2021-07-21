20b16f42f4fb91c32d51e66eb224dd0640260810
Andrey Tismenetski <tisandrey@gmail.com>
2021-07-20 20:25:50 -0500

    Initial commit

--- src/main/java/Utils/ShareDriver.java
+++ src/main/java/Utils/ShareDriver.java
@@ -11,18 +11,20 @@ import java.util.concurrent.TimeUnit;
 public class ShareDriver {
     public static WebDriver webDriver;
 
-    public static WebDriver getWebDriver() {
+    public static WebDriver getWebDriver(){
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // чтобы проект мог бежать в Jenkins( на Linux)
-        if (webDriver == null) {
-            WebDriverManager.chromedriver().setup();
-            webDriver = new ChromeDriver();
-            webDriver.manage().window().maximize();
-            webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        }
-        return webDriver;
-    }
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+ if (webDriver == null) {
 
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+        return webDriver;
+}
     public static void closeDriver() {
         if (webDriver != null) {
             webDriver.close();
